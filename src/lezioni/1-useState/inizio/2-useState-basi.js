4d0e15226d5001c57b6bf58743c0c84ae43d7174
marcocareddu <saldumaccu@tuta.io>
2024-01-10 11:26:34 +0100

    React Hooks Excercise

--- src/lezioni/1-useState/inizio/2-useState-basi.js
+++ src/lezioni/1-useState/inizio/2-useState-basi.js
@@ -1,17 +1,21 @@
 import React from "react";
-// Gli Hooks devono essere usasti con use davanti
-// componenti devono avere la prima lettere Maiuscola
-// Gli Hook devono essere invocati all'interno del corpo/funzione del componente
-// Gli Hook non possono essere utilizzati in maniera condizionale
+import { useState } from "react";
 
 const UsoBase = () => {
-  //   const value = useState()[0];
-  //   const handler = useState()[1];
-  //   console.log(value, handler);
+  const [title, setTitle] = useState("Titolo Banale")
+
+  const changeTitle = () =>{
+    if(title === "Titolo Banale") {
+      setTitle("Titolo Magico")
+    } else {
+      setTitle("Titolo Banale")
+    }
+  }
 
   return (
     <>
-      <h2> Basi useState</h2>
+      <h2>{title}</h2>
+      <button className="btn btn-success shadow" onClick={changeTitle}>Cliccami</button>
     </>
   );
 };
